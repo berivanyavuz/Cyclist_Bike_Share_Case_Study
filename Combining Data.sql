98774a6d131ab12044a03819c96b290bed74d2cd
berivanyavuz <berivanyvz@gmail.com>
2024-08-26 14:35:50 +0200

    Update Combining Data.sql

--- Combining Data.sql
+++ Combining Data.sql
@@ -1,1 +1,27 @@
-dfklshfjsdmfdsfjlf
+-- combining data
+
+create table 'genuine-ember-407810.Cylistic_Bike_Share_Analyze_24' as (
+  
+  SELECT * FROM genuine-ember-407810.Cyclistic_Bike_Share_Case_Study.202101-divvy-tripdata
+  UNION ALL
+  SELECT * FROM genuine-ember-407810.Cyclistic_Bike_Share_Case_Study.202102-divvy-tripdata
+  UNION ALL
+  SELECT * FROM genuine-ember-407810.Cyclistic_Bike_Share_Case_Study.202103-divvy-tripdata
+  UNION ALL
+  SELECT * FROM genuine-ember-407810.Cyclistic_Bike_Share_Case_Study.202104-divvy-tripdata
+  UNION ALL
+  SELECT * FROM genuine-ember-407810.Cyclistic_Bike_Share_Case_Study.202105-divvy-tripdata
+  UNION ALL
+  SELECT * FROM genuine-ember-407810.Cyclistic_Bike_Share_Case_Study.202106-divvy-tripdata
+  UNION ALL
+  SELECT * FROM genuine-ember-407810.Cyclistic_Bike_Share_Case_Study.202107-divvy-tripdata
+  UNION ALL
+  SELECT* FROM genuine-ember-407810.Cyclistic_Bike_Share_Case_Study.202108-divvy-tripdata
+  UNION ALL
+  SELECT * FROM genuine-ember-407810.Cyclistic_Bike_Share_Case_Study.202109-divvy-tripdata
+  UNION ALL
+  SELECT * FROM genuine-ember-407810.Cyclistic_Bike_Share_Case_Study.202110-divvy-tripdata
+  UNION ALL
+  SELECT * FROM genuine-ember-407810.Cyclistic_Bike_Share_Case_Study.202111-divvy-tripdata
+  UNION ALL
+  SELECT * FROM genuine-ember-407810.Cyclistic_Bike_Share_Case_Study.202112-divvy-tripdata
